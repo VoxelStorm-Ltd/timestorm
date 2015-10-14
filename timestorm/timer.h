f387a0b64f029e5ab38adf8bf596caede2a65fb7
slowriot <slowriot@voxelstorm.com>
2015-10-14 11:07:18 +0300

    Re-organising struct to minimise padding (part 4)

--- timestorm/timer.h
+++ timestorm/timer.h
@@ -20,11 +20,11 @@ enum class timescale {
 template<typename T>
 class timer {
 public:
-  timescale scale = timescale::AUTO;                                            // on what timescale to report the results
+  std::chrono::time_point<std::chrono::system_clock> time_start = std::chrono::system_clock::now();
+
   std::string prefix;                                                           // what to output when finished before the time value
   std::string suffix;                                                           // what to output when finished after the time value
-
-  std::chrono::time_point<std::chrono::system_clock> time_start = std::chrono::system_clock::now();
+  timescale scale = timescale::AUTO;                                            // on what timescale to report the results
 
   timer(timescale scale = timescale::AUTO,
         std::string const &message_pre  = "Done in ",
