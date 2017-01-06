883e9b33f5347a6f27fc96e1b2d064a9dd07e570
SlowRiot <slowriot@voxelstorm.com>
2017-01-06 12:30:12 +0000

    adding forward declaration header

---
+++ timestorm/timestorm_forward.h
@@ -0,0 +1,11 @@
+#ifndef TIMESTORM_FORWARD_H_INCLUDED
+#define TIMESTORM_FORWARD_H_INCLUDED
+
+namespace timestorm {
+
+template<typename T> class timer;
+enum class timescale;
+
+}
+
+#endif // TIMESTORM_FORWARD_H_INCLUDED
